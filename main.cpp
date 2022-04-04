47fcc4693c968d476f2770af00c301ed5da08d0d
5242360 <100364230+5242360@users.noreply.github.com>
2022-04-04 15:31:35 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n,a=0,b=1, g=0;
+    cout<<"inserire un valore\n";
+    cin>>n;
+    while(g<n){
+        g=a+b;
+        b=a;
+        a=g;
+        if(g<=n){
+            cout<<g<<endl;
+        }
+    }
+
+    return 0;
 }
